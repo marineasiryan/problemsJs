9f89cd274b25f5bff76e99f4c77adb2e576a54d8
marineasiryan <marinasiryan14@gmail.com>
2022-09-20 13:12:45 +0400

    commit

---
+++ mostLeastBit.js
@@ -0,0 +1,21 @@
+function mostLeast(dec) {
+    if (dec === 0) {
+        return 0;
+    }
+    let bin = (dec >>> 0).toString(2).padStart(32, "0");
+    console.log("the binary number is ", bin);
+
+    let lastDigitStr = String(bin).slice(-1);
+    let lastDigitNum = Number(lastDigitStr);
+    console.log("last digit is ", lastDigitNum);
+
+    let firstDigitStr = String(bin)[0];
+    let firstDigitNum = Number(firstDigitStr);
+    console.log("first digit is ", firstDigitNum);
+
+    if (lastDigitNum === firstDigitNum) {
+        return true;
+    }
+    return false;
+}
+console.log(mostLeast(10));
